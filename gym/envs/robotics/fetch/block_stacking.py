dd76ea818ec900a78d4a593fd80bca65b75b47a6
Seungjae Jung <seanexplode@gmail.com>
2018-08-14 22:26:39 +0900

    add xml and python file for block stacking

---
+++ gym/envs/robotics/fetch/block_stacking.py
@@ -0,0 +1,22 @@
+from gym import utils
+from gym.envs.robotics import fetch_env
+
+
+class FetchBlockStacking(fetch_env.FetchEnv, utils.EzPickle):
+    def __init__(self, reward_type='sparse'):
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+            'object1:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+            'object2:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+            'object3:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+            'object4:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+        }
+        fetch_env.FetchEnv.__init__(
+            self, 'fetch/block_stacking.xml', has_object=True, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
+            obj_range=0.15, target_range=0.15, distance_threshold=0.05,
+            initial_qpos=initial_qpos, reward_type=reward_type)
+        utils.EzPickle.__init__(self)
