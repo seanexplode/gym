5d4ef6d89f9a7a91674c8911f4a1a22893644686
Seungjae Jung <seanexplode@gmail.com>
2018-09-13 19:06:49 +0900

    now there are six blocks

--- gym/envs/robotics/fetch/block_stacking.py
+++ gym/envs/robotics/fetch/block_stacking.py
@@ -5,18 +5,19 @@ from random import uniform
 
 class FetchBlockStacking(fetch_env.FetchEnv, utils.EzPickle):
     def __init__(self, reward_type='sparse'):
-        var = 0.25
+        var = 0.22
         initial_qpos = {
             'robot0:slide0': 0.405,
             'robot0:slide1': 0.48,
             'robot0:slide2': 0.0,
             # qpos[0~2] : root joint cartesian position (x, y, z) of center of mass
             # qpos[3~6] : orientation
-            'object0:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.42, 1., 0., 0., 0.],
-            'object1:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.42, 1., 0., 0., 0.],
-            'object2:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.42, 1., 0., 0., 0.],
-            'object3:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.42, 1., 0., 0., 0.],
-            'object4:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+            'object0:joint': [1.25 - uniform(-var, var), 0.8 - uniform(-var, var), 0.43, 1., 0., 0., 0.],
+            'object1:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.43, 1., 0., 0., 0.],
+            'object2:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.43, 1., 0., 0., 0.],
+            'object3:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.43, 1., 0., 0., 0.],
+            'object4:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.43, 1., 0., 0., 0.],
+            'object5:joint': [1.25 - uniform(-var, var), 0.7 - uniform(-var, var), 0.43, 1., 0., 0., 0.],
         }
         fetch_env.FetchEnv.__init__(
             self, 'fetch/block_stacking.xml', has_object=True, block_gripper=False, n_substeps=20,
@@ -28,3 +29,5 @@ class FetchBlockStacking(fetch_env.FetchEnv, utils.EzPickle):
         #  print("com of object 0", self.sim.data.get_site_xpos('object0'))
         #  print("com of object 1", self.sim.data.get_site_xpos('object1'))
         #  print("com of object 2", self.sim.data.get_site_xpos('object2'))
+
+        print("body com of table", self.sim.data.get_body_xipos('table0'))
